04bd19d86a19b0a1a201f952cdf3818cd2e0560e
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-10-01 16:29:06 -0400

    iam roles

--- main.tf
+++ main.tf
@@ -4,20 +4,20 @@ provider "aws" {
 
 # To create a new IAM role
 resource "aws_iam_role" "jayaworld-iam-role" {
-  name = var.iam_role_name
-  path = "/"
-  permissions_boundary = var.iam_role_permissions_boundary
-  description = var.iam_role_description
+  name                  = var.iam_role_name
+  path                  = "/"
+  permissions_boundary  = var.iam_role_permissions_boundary
+  description           = var.iam_role_description
   force_detach_policies = var.force_detach_policies
-  assume_role_policy = var.assume_role_policy
-  managed_policy_arns = var.managed_policy_arns
+  assume_role_policy    = var.assume_role_policy
+  managed_policy_arns   = var.managed_policy_arns
   dynamic "inline_policy" {
     for_each = var.iamrole_inline_policy == null ? [] : var.iamrole_inline_policy
     content {
-    name = lookup(inline_policy.value.name, null)
-    policy = lookup(inline_policy.value.policy, null)
+      name   = lookup(inline_policy.value.name, null)
+      policy = lookup(inline_policy.value.policy, null)
     }
   }
   max_session_duration = var.max_session_duration
-  tags = merge(var.tags)
+  tags                 = merge(var.tags)
 }
