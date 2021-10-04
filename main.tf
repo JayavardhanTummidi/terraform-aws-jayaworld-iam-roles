c02b48dc3e2198c41ed6edbf178676b25a8c3f76
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-10-04 18:02:05 -0400

    iam roles

--- main.tf
+++ main.tf
@@ -15,10 +15,33 @@ resource "aws_iam_role" "jayaworld-iam-role" {
     for_each = var.inline_policy
 
     content {
-      name   = inline_policy.value.name
-      policy = inline_policy.value.policy
+      name   = lookup(inline_policy.value, "name", null)
+      policy = lookup(inline_policy.value, "policy", null)
     }
   }
   max_session_duration = var.max_session_duration
   tags                 = merge(var.tags)
+}
+
+# To create IAM service linked role
+resource "aws_iam_service_linked_role" "jayaworld-iam-service-linked-role" {
+  count            = var.create_iam_service_linked_role ? 1 : 0
+  description      = var.iam_role_description
+  aws_service_name = var.aws_service_name
+}
+
+# To create SAML Provider
+resource "aws_iam_saml_provider" "jayaworld-iam-saml-provider" {
+  count                  = var.create_iam_saml_provider ? 1 : 0
+  name                   = var.saml_provider_name
+  saml_metadata_document = var.saml_metadata_document
+}
+
+# To create IAM OpenID Connect provider.
+resource "aws_iam_openid_connect_provider" "jayaworld-openid-connect-provider" {
+  count           = var.create_iam_oidc ? 1 : 0
+  url             = var.oidc_url
+  client_id_list  = var.oidc_client_id_list
+  thumbprint_list = var.oidc_thumbprint_list
+  tags            = merge(var.tags)
 }
